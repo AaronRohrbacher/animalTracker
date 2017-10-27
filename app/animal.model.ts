cce6ab6e23cb87bcd6c0707f30fe8a55fc8253f9
Aaron Rohrbacher <arjazz@icloud.com>
2017-10-27 10:46:11 -0700

    set up model, components, and module

---
+++ app/animal.model.ts
@@ -0,0 +1,3 @@
+export class Keg {
+  constructor(public name: string, public species: string, public age: number, public diet: string, public location: string, public caretakers: number, sex: string, likes: string, dislikes: string) {}
+}
