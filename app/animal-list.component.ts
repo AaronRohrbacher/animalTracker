8e3e772dccf0ae13a919e42ea68c8f86ebef57a6
Aaron Rohrbacher <arjazz@icloud.com>
2017-10-27 15:42:14 -0700

    tidy front end code

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -4,40 +4,43 @@ import { Animal } from './animal.model';
 @Component({
   selector: 'animal-list',
   template: `
-  <select (change)="rangeChange($event.target.value)">
-    <option value ="all">All Animals</option>
-    <option value ="young">Young Animals</option>
-    <option value ="old">Mature Animals</option>
-  </select>
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Species</th>
-        <th>Name</th>
-        <th>Age</th>
-        <th>Diet</th>
-        <th>Location</th>
-        <th>Caretakers</th>
-        <th>Sex</th>
-        <th>Likes</th>
-        <th>Dislikes</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr *ngFor="let animal of childAnimalList | age: range">
-        <td> {{animal.species}} </td>
-        <td> {{animal.name}} </td>
-        <td> {{animal.age}} </td>
-        <td> {{animal.diet}} </td>
-        <td> {{animal.location}} </td>
-        <td> {{animal.caretakers}} </td>
-        <td> {{animal.sex}} </td>
-        <td> {{animal.likes}} </td>
-        <td> {{animal.dislikes}} </td>
-        <td><button (click)="editButton(animal)">Edit</button>
-      </tr>
-    </tbody>
-  </table>
+  <div>
+    <label>View:</label>
+    <select (change)="rangeChange($event.target.value)">
+      <option value ="all">All Animals</option>
+      <option value ="young">Young Animals</option>
+      <option value ="old">Mature Animals</option>
+    </select>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Species</th>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Diet</th>
+          <th>Location</th>
+          <th>Caretakers</th>
+          <th>Sex</th>
+          <th>Likes</th>
+          <th>Dislikes</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr *ngFor="let animal of childAnimalList | age: range">
+          <td> {{animal.species}} </td>
+          <td> {{animal.name}} </td>
+          <td> {{animal.age}} </td>
+          <td> {{animal.diet}} </td>
+          <td> {{animal.location}} </td>
+          <td> {{animal.caretakers}} </td>
+          <td> {{animal.sex}} </td>
+          <td> {{animal.likes}} </td>
+          <td> {{animal.dislikes}} </td>
+          <td><button (click)="editButton(animal)">Edit</button>
+        </tr>
+      </tbody>
+    </table>
+  </div>
   `
 })
 
